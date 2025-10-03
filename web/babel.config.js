8635c117cb3da46a6da2c74e36dcd2dc39e4554c
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-03 18:27:11 -0300

    feat: refactor babel.config.js to improve plugin and preset management

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,6 +1,31 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const isTest = process.env.NODE_ENV === 'test';
+
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ];
+
+  // Adiciona NativeWind e Reanimated apenas fora dos testes
+  if (!isTest) {
+    plugins.push('react-native-reanimated/plugin');
+  }
+
+  const presets = isTest
+    ? ['babel-preset-expo']
+    : [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'];
+
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets,
+    plugins,
   };
 };
