80b15d0f137b337f1dc784ec7dc38fafde61d5cf
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-08 19:31:55 -0300

    feat: add useGetEnderecosBySearch hook for fetching enderecos with infinite scrolling

---
+++ web/src/hooks/queries/enderecos/useGetEnderecosBySearch.tsx
@@ -0,0 +1,22 @@
+import { enderecosApi } from '@/api/enderecosApi';
+import { EnderecosFilterQuery } from '@/types/enderecos/filtersQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useGetEnderecosBySearch = (filters: Omit<EnderecosFilterQuery, 'skip'>) => {
+  return useInfiniteQuery({
+    queryKey: ['useGetEnderecosBySearch', filters],
+    queryFn: ({ pageParam }) =>
+      enderecosApi.getEnderecos({
+        ...filters,
+        skip: pageParam,
+      }),
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length < (filters.take || 20)) {
+        return undefined;
+      }
+      return lastPageParam + (filters.take || 20);
+    },
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 5,
+  });
+};
