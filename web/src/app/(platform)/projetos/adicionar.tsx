20b31c03aa66c0aa2c57868e95ecf2f7b0f41ea2
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-08 19:30:29 -0300

    feat: create ProjetosAdicionarScreen component with basic layout

---
+++ web/src/app/(platform)/projetos/adicionar.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+export default function ProjetosAdicionarScreen() {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
