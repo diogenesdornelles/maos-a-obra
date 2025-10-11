f77789e5855a7d1b043d8fa7ed76b88de295232e
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-10 21:34:50 -0300

    feat: add useGetItensBySearch hook for fetching items with optional filters

---
+++ web/src/hooks/queries/itens/useGetItensBySearch.ts
@@ -0,0 +1,23 @@
+import { itensApi } from '@/api/ itensApi';
+import { ItensFilterQuery } from '@/types/itens/filtersQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useGetItensBySearch = (filters?: Omit<ItensFilterQuery, 'skip'>) => {
+  return useInfiniteQuery({
+    queryKey: ['useGetItensBySearch', filters],
+    queryFn: ({ pageParam }) =>
+      itensApi.getItens({
+        ...(filters &&
+          Object.fromEntries(Object.entries(filters).filter(([_, v]) => v !== undefined))),
+        skip: pageParam,
+      } as ItensFilterQuery),
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length < (filters?.take || 20)) {
+        return undefined;
+      }
+      return lastPageParam + (filters?.take || 20);
+    },
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 5,
+  });
+};
