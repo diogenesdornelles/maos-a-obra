338a380ed7eb1220d6e840e07f9e28818ab9478b
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-10 21:35:22 -0300

    feat: add EstadoProps interface for state representation

---
+++ web/src/types/estados/estados.ts
@@ -0,0 +1,10 @@
+export interface EstadoProps {
+  id: string;
+  codigoUf: string;
+  nome: string;
+  uf: string;
+  regiao: string;
+  status: boolean;
+  criadoEm: string;
+  atualizadoEm: string;
+}
