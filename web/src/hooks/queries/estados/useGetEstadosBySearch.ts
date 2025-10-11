9157d0a3b94cb612a04dde24115b0dab85f12b02
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-10 21:34:44 -0300

    feat: add useGetEstadosBySearch hook for fetching states with optional filters

---
+++ web/src/hooks/queries/estados/useGetEstadosBySearch.ts
@@ -0,0 +1,23 @@
+import { estadoApi } from '@/api/estadosApi';
+import { EstadosFilterQuery } from '@/types/estados/filtersQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useGetEstadosBySearch = (filters?: Omit<EstadosFilterQuery, 'skip'>) => {
+  return useInfiniteQuery({
+    queryKey: ['useGetEstadosBySearch', filters],
+    queryFn: ({ pageParam }) =>
+      estadoApi.getEstados({
+        ...(filters &&
+          Object.fromEntries(Object.entries(filters).filter(([_, v]) => v !== undefined))),
+        skip: pageParam,
+      } as EstadosFilterQuery),
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length < (filters?.take || 20)) {
+        return undefined;
+      }
+      return lastPageParam + (filters?.take || 20);
+    },
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 5,
+  });
+};
