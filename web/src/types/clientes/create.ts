77a756182fcecbf030f5f671192ff18cc526d007
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-12 21:58:01 -0300

    refactor: simplify CreateCliente and CreatedCliente interfaces by making properties nullable and optional

--- web/src/types/clientes/create.ts
+++ web/src/types/clientes/create.ts
@@ -1,40 +1,28 @@
+import { BaseProps } from "../base";
+import { EnderecoProps } from "../enderecos/enderecos";
+
 export interface CreateCliente {
+  enderecoId?: string | null;
   nome: string;
-  enderecoId?: string;
-  sobrenome?: string;
-  cpf?: string;
-  cnpj?: string;
-  nascimento?: string;
-  telefone?: string;
-  email?: string;
+  sobrenome?: string | null;
+  cpf?: string | null;
+  cnpj?: string | null;
+  nascimento?: string | null;
+  telefone?: string | null;
+  email?: string | null;
   status?: boolean;
 }
 
-export interface CreatedCliente {
-  id: string;
-  enderecoId: string;
+export interface CreatedCliente extends BaseProps {
+  enderecoId: string | null;
   usuarioId: string;
   nome: string;
-  sobrenome: string;
-  cpf: string;
-  cnpj: string;
-  nascimento: string;
-  telefone: string;
-  email: string;
-  status: boolean;
-  criadoEm: string;
-  atualizadoEm: string;
-  endereco: {
-    id: string;
-    logradouro: string;
-    numero: string;
-    complemento: string;
-    usuarioId: string;
-    bairroId: string;
-    pais: string;
-    cep: string;
-    status: boolean;
-    criadoEm: string;
-    atualizadoEm: string;
-  };
+  sobrenome: string | null;
+  cpf: string | null;
+  cnpj: string | null;
+  nascimento: string | null;
+  telefone: string | null;
+  email: string | null;
+  status?: boolean;
+  endereco: EnderecoProps | null;
 }
