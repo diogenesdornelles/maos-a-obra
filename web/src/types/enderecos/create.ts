27c0edf87a496386e3697bde059b54a0fd103a14
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-12 21:58:13 -0300

    refactor: make properties in CreateEndereco optional and simplify CreatedEndereco structure

--- web/src/types/enderecos/create.ts
+++ web/src/types/enderecos/create.ts
@@ -1,25 +1,16 @@
+import { BairrosProps } from "../bairros/bairros";
+
 export interface CreateEndereco {
   bairroId: string;
   logradouro: string;
-  numero: string | null;
-  complemento: string | null;
-  pais: string | null;
-  cep: string | null;
-  status: boolean;
+  numero?: string | null;
+  complemento?: string | null;
+  pais?: string | null;
+  cep?: string | null;
+  status?: boolean;
 }
 
 export interface CreatedEndereco extends CreateEndereco {
-  id: string;
-  criadoEm: string;
-  atualizadoEm: string;
   usuarioId: string;
-  bairro: {
-    id: string;
-    criadoEm: string;
-    atualizadoEm: string;
-    codigo: string;
-    nome: string;
-    uf: string;
-    status: true;
-  };
+  bairro: BairrosProps;
 }
