e024f203fe334ffafa5d76eac44b87d592b14d06
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-12 21:57:26 -0300

    feat: add useGetProjetosBySearch hook for fetching projects with filters

---
+++ web/src/hooks/queries/projetos/useGeProjetosBySearch.tsx
@@ -0,0 +1,23 @@
+import { projetosApi } from '@/api/projetosApi';
+import { ProjetosFilterQuery } from '@/types/projetos/filtersQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useGetProjetosBySearch = (filters?: Omit<ProjetosFilterQuery, 'skip'>) => {
+  return useInfiniteQuery({
+    queryKey: ['useGetProjetosBySearch', filters],
+    queryFn: ({ pageParam }) =>
+      projetosApi.getProjetos({
+        ...(filters &&
+          Object.fromEntries(Object.entries(filters).filter(([_, v]) => v !== undefined))),
+        skip: pageParam,
+      } as ProjetosFilterQuery),
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length < (filters?.take || 20)) {
+        return undefined;
+      }
+      return lastPageParam + (filters?.take || 20);
+    },
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 5,
+  });
+};
