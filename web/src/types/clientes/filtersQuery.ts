3bd50d3c65e258d216063510e0ee1f8e9bc6ce8d
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-12 21:58:05 -0300

    refactor: add optional properties to ClientesFilterQuery interface

--- web/src/types/clientes/filtersQuery.ts
+++ web/src/types/clientes/filtersQuery.ts
@@ -2,7 +2,10 @@ import { DefaultFilters } from '../default';
 
 export interface ClientesFilterQuery extends DefaultFilters {
   nome?: string;
+  sobrenome?: string;
   cpf?: string;
   cnpj?: string;
   email?: string;
+  telefone?: string;
+  status?: string;
 }
