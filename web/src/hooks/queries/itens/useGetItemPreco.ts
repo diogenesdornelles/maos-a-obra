d797122ea485a216c97c62b6948bca00f8c471d3
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:51:25 -0300

    feat: remove staleTime setting from useGetItemPreco hook

--- web/src/hooks/queries/itens/useGetItemPreco.ts
+++ web/src/hooks/queries/itens/useGetItemPreco.ts
@@ -6,7 +6,6 @@ export const useGetItemPreco = (filters?: ItensPrecoFilterQuery) => {
   return useQuery({
     queryKey: ['useGetItemPreco', filters],
     queryFn: () => itensApi.getItemPreco(filters),
-    staleTime: 1000 * 60 * 5,
     enabled: !!(filters?.estadoId && filters?.itemId),
   });
 };
