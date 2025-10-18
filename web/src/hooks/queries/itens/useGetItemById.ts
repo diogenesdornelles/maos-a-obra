9ba45879a06eac3d32bed219352166712edf7dd1
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:51:21 -0300

    feat: remove staleTime setting from useGetItemById hook

--- web/src/hooks/queries/itens/useGetItemById.ts
+++ web/src/hooks/queries/itens/useGetItemById.ts
@@ -1,11 +1,10 @@
-import { itensApi } from "@/api/ itensApi";
-import { useQuery } from "@tanstack/react-query";
+import { itensApi } from '@/api/ itensApi';
+import { useQuery } from '@tanstack/react-query';
 
 export function useGetItemById({ id }: { id: string }) {
   return useQuery({
     queryKey: ['useGetItemById', id],
     queryFn: () => itensApi.getItemById(id),
-    staleTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
     enabled: !!id,
   });
