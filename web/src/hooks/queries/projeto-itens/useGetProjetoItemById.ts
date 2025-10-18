908c3ed9c2ea7595ef5b1b0c42045c2e009f0343
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:51:31 -0300

    feat: remove staleTime setting from useGetProjetoItemById hook

--- web/src/hooks/queries/projeto-itens/useGetProjetoItemById.ts
+++ web/src/hooks/queries/projeto-itens/useGetProjetoItemById.ts
@@ -1,12 +1,10 @@
-import { projetoItensApi } from "@/api/projetoItensApi";
-import { projetosApi } from "@/api/projetosApi";
-import { useQuery } from "@tanstack/react-query";
+import { projetoItensApi } from '@/api/projetoItensApi';
+import { useQuery } from '@tanstack/react-query';
 
 export function useGetProjetoItemById({ id }: { id: string }) {
   return useQuery({
     queryKey: ['useGetProjetoItemById', id],
     queryFn: () => projetoItensApi.getProjetoItemById(id),
-    staleTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
     enabled: !!id,
   });
