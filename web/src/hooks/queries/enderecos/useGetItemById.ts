aaf1770f454e44a3d8f2537f278a85d6f410613a
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:51:11 -0300

    feat: remove staleTime setting from useGetEnderecoById hook

--- web/src/hooks/queries/enderecos/useGetItemById.ts
+++ web/src/hooks/queries/enderecos/useGetItemById.ts
@@ -1,11 +1,10 @@
-import { enderecosApi } from "@/api/enderecosApi";
-import { useQuery } from "@tanstack/react-query";
+import { enderecosApi } from '@/api/enderecosApi';
+import { useQuery } from '@tanstack/react-query';
 
 export function useGetEnderecoById({ id }: { id: string }) {
   return useQuery({
     queryKey: ['useGetEnderecoById', id],
     queryFn: () => enderecosApi.getEnderecoById(id),
-    staleTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
     enabled: !!id,
   });
