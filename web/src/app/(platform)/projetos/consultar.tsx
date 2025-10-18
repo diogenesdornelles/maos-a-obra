94a385c1907b0319b58e321e4704ad1806b7fa47
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:50:18 -0300

    feat: update ProjetosConsultarScreen to include header and margin styling

--- web/src/app/(platform)/projetos/consultar.tsx
+++ web/src/app/(platform)/projetos/consultar.tsx
@@ -1,5 +1,11 @@
+import { Stack } from 'expo-router';
 import { View } from 'react-native';
 
 export default function ProjetosConsultarScreen() {
-  return <View></View>;
+  return (
+    <>
+      <Stack.Screen options={{ headerTitle: 'Projetos / consultar' }} />
+      <View className="m-5"></View>
+    </>
+  );
 }
