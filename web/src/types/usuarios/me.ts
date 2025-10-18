7dd74efec4cd914a07187f631177cd0140092315
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-18 10:51:49 -0300

    feat: add MeProps interface for user data structure

---
+++ web/src/types/usuarios/me.ts
@@ -0,0 +1,13 @@
+export interface MeProps {
+  id: string;
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  nascimento: string;
+  email: string;
+  funcao: string;
+  status: boolean;
+  criadoEm: string;
+  atualizadoEm: string;
+  projetos: any;
+}
