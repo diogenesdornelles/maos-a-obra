e354f4ee7410d8b5c528f14e61eb9e88691fa664
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-22 22:31:24 -0300

    fire: remove unused client query hooks for search and creation

--- web/src/hooks/queries/clients/usePostCreateClient.ts
+++
@@ -1,9 +0,0 @@
-import { clientesApi } from '@/api/clientesApi';
-import { CreateCliente } from '@/types/clientes/create';
-import { useMutation } from '@tanstack/react-query';
-
-export const usePostCreateClient = () => {
-  return useMutation({
-    mutationFn: (clientData: CreateCliente) => clientesApi.createCliente(clientData),
-  });
-};
