c681bda6a009eb388c3c038b2db4a21bc4e2cdb4
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-22 22:32:24 -0300

    fix: reorder imports and add EstadoProps to ProjetoProps interface

--- web/src/types/projetos/projetos.ts
+++ web/src/types/projetos/projetos.ts
@@ -1,12 +1,13 @@
-import { BaseProps } from "../base";
-import { ClienteProps } from "../clientes/clientes";
-import { ProjetoItemProps } from "../projeto-itens/projetoItens";
-import { UpdateProjeto } from "./update";
-
+import { BaseProps } from '../base';
+import { ClienteProps } from '../clientes/clientes';
+import { EstadoProps } from '../estados/estados';
+import { ProjetoItemProps } from '../projeto-itens/projetoItens';
+import { UpdateProjeto } from './update';
 
 export interface ProjetoProps extends BaseProps {
   clienteId: string;
   estadoId: string;
+  estado: EstadoProps;
   usuarioId: string;
   nome: string;
   valorTotal: string;
