e9e5cc9f98389f3221a5e8b19837e7f24d1bb008
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-22 22:31:12 -0300

    feat: add useGetClientesBySearch hook for client search functionality

---
+++ web/src/hooks/queries/clients/useGetClientesBySearch.ts
@@ -0,0 +1,22 @@
+import { clientesApi } from '@/api/clientesApi';
+import { ClientesFilterQuery } from '@/types/clientes/filtersQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useGetClientesBySearch = (filters?: Omit<ClientesFilterQuery, 'skip'>) => {
+  return useInfiniteQuery({
+    queryKey: ['useGetClientesBySearch', filters],
+    queryFn: ({ pageParam }) =>
+      clientesApi.getClientes({
+        ...(filters &&
+          Object.fromEntries(Object.entries(filters).filter(([_, v]) => v !== undefined))),
+        skip: pageParam,
+      } as ClientesFilterQuery),
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length < (filters?.take || 20)) {
+        return undefined;
+      }
+      return lastPageParam + (filters?.take || 20);
+    },
+    initialPageParam: 0,
+  });
+};
