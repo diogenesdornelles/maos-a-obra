32cf94a3f2d3017776801fecad6e8b72217678e0
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-10-22 22:30:10 -0300

    feat: add update and delete functions to projetoItensApi for project item management

--- web/src/api/projetoItensApi.ts
+++ web/src/api/projetoItensApi.ts
@@ -2,6 +2,7 @@ import { restClient } from '@/services/restClient';
 import { CreatedProjetoItem, CreateProjetoItem } from '@/types/projeto-itens/create';
 import { ProjetoItensFilterQuery } from '@/types/projeto-itens/filtersQuery';
 import { ProjetoItemProps } from '@/types/projeto-itens/projetoItens';
+import { UpdatedProjetoItem } from '@/types/projeto-itens/update';
 
 export const projetoItensApi = {
   createProjetoItem: async (body: CreateProjetoItem) => {
@@ -16,13 +17,13 @@ export const projetoItensApi = {
     if (params?.take !== undefined) searchParams.set('take', String(params.take));
     if (params?.quantidade) searchParams.set('quantidade', String(params.quantidade));
     if (params?.status) searchParams.set('status', String(params.status));
+    if (params?.orderBy) searchParams.set('orderBy', String(params.orderBy));
+    if (params?.orderDir) searchParams.set('orderDir', String(params.orderDir));
     if (params?.projetoId) searchParams.set('projetoId', params.projetoId);
 
     const data = await restClient.get<ProjetoItemProps[]>(
       `/projeto-itens/search?${searchParams.toString()}`
     );
-
-    console.log('teste: ', data);
 
     return data;
   },
@@ -30,6 +31,16 @@ export const projetoItensApi = {
   getProjetoItemById: async (id: string) => {
     const data = await restClient.get<ProjetoItemProps>(`/projeto-itens/${id}`);
 
+    return data;
+  },
+
+  updateProjetoItem: async (body: UpdatedProjetoItem) => {
+      const data = await restClient.patch<CreatedProjetoItem>(`/projeto-itens`, body);
+      return data;
+    },
+
+  deleteProjetoItem: async (id: string) => {
+    const data = await restClient.delete<CreatedProjetoItem>(`/projeto-itens/${id}`);
     return data;
   },
 };
