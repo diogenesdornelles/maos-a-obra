cfaef55e1723a3f4c53937be2ff1ce7afd728d80
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-04 23:39:14 -0300

    fix: update useUpdateEndereco to handle partial updates with body and id parameters

--- web/src/hooks/queries/enderecos/useUpdateEndereco.ts
+++ web/src/hooks/queries/enderecos/useUpdateEndereco.ts
@@ -1,6 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { enderecosApi } from '@/api/enderecosApi';
+import { UpdateEndereco } from '@/types/enderecos/update';
 
 export function useUpdateEndereco() {
-	return useMutation({ mutationFn: enderecosApi.updateEndereco });
+	return useMutation({ 
+		mutationFn: ({ body, id }: { body: Partial<UpdateEndereco>; id: string }) => 
+			enderecosApi.updateEndereco(body, id) 
+	});
 }
