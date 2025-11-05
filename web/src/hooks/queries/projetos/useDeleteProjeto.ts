b8309d140fe46689ef72c2883f3a77aab03da62b
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-04 23:39:29 -0300

    fix: add success handling and query invalidation for projeto deletion in useDeleteProjeto

--- web/src/hooks/queries/projetos/useDeleteProjeto.ts
+++ web/src/hooks/queries/projetos/useDeleteProjeto.ts
@@ -1,6 +1,13 @@
 import { projetosApi } from '@/api/projetosApi';
+import { queryClient } from '@/services/queryClient';
 import { useMutation } from '@tanstack/react-query';
 
 export function useDeleteProjeto() {
-	return useMutation({ mutationFn: projetosApi.deleteProjeto });
+	return useMutation({ 
+		mutationFn: projetosApi.deleteProjeto,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetProjetosBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetProjetoById'], exact: false });
+		}
+	});
 }
