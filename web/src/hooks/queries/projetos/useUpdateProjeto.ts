1643f8a5d20ee6b83ded9f08da7d394ff5bccaaf
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-04 23:39:32 -0300

    fix: update useUpdateProjeto to handle partial updates with body and id parameters

--- web/src/hooks/queries/projetos/useUpdateProjeto.ts
+++ web/src/hooks/queries/projetos/useUpdateProjeto.ts
@@ -1,6 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { projetosApi } from '@/api/projetosApi';
+import { UpdateProjeto } from '@/types/projetos/update';
 
 export function useUpdateProjeto() {
-	return useMutation({ mutationFn: projetosApi.updateProjeto });
+	return useMutation({ 
+		mutationFn: ({ body, id }: { body: Partial<UpdateProjeto>; id: string }) => 
+			projetosApi.updateProjeto(body, id) 
+	});
 }
