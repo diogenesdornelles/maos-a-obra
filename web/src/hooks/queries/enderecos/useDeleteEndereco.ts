e04de8ba10780c8f6e805712d52824fd177734c0
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-04 23:39:10 -0300

    fix: add success handling and query invalidation for endereco deletion in useDeleteEndereco

--- web/src/hooks/queries/enderecos/useDeleteEndereco.ts
+++ web/src/hooks/queries/enderecos/useDeleteEndereco.ts
@@ -1,6 +1,14 @@
 import { enderecosApi } from '@/api/enderecosApi';
+import { queryClient } from '@/services/queryClient';
 import { useMutation } from '@tanstack/react-query';
 
 export function useDeleteEndereco() {
-	return useMutation({ mutationFn: enderecosApi.deleteEndereco });
+	
+	return useMutation({ 
+		mutationFn: enderecosApi.deleteEndereco,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetEnderecosBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetEnderecoById'], exact: false });
+		}
+	});
 }
