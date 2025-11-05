ca286519988e764799c0aa86047db0c80702b07e
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-04 23:39:01 -0300

    fix: add success handling for client deletion with query invalidation in useDeleteClient

--- web/src/hooks/queries/clients/useDeleteCliente.ts
+++ web/src/hooks/queries/clients/useDeleteCliente.ts
@@ -1,7 +1,15 @@
 
 import { clientesApi } from '@/api/clientesApi';
+import { queryClient } from '@/services/queryClient';
 import { useMutation } from '@tanstack/react-query';
 
 export function useDeleteClient() {
-	return useMutation({ mutationFn: clientesApi.deleteCliente });
+	
+	return useMutation({ 
+		mutationFn: clientesApi.deleteCliente,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetClientesBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetClienteById'], exact: false });
+		}
+	});
 }
