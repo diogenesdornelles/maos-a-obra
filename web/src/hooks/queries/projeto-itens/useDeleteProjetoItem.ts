38b52d999c1c0041b149f9d2212bbb91d21a4a6b
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:21 -0300

    feat: simplify onSuccess handler to invalidate queries after deleting projeto item

--- web/src/hooks/queries/projeto-itens/useDeleteProjetoItem.ts
+++ web/src/hooks/queries/projeto-itens/useDeleteProjetoItem.ts
@@ -6,23 +6,9 @@ import { ProjetoItemProps } from '@/types/projeto-itens/projetoItens';
 export function useDeleteProjetoItem() {
   return useMutation({
     mutationFn: projetoItensApi.deleteProjetoItem,
-    onSuccess: (_data, deletedId) => {
-      queryClient.setQueriesData(
-        { queryKey: ['useGetProjetoItensBySearch'], exact: false },
-        (old:
-          | InfiniteData<ProjetoItemProps[]>
-          | undefined) => {
-          if (!old) return old;
-          return {
-            ...old,
-            pages: old.pages.map((page) => page.filter((it) => it.id !== deletedId)),
-          };
-        }
-      );
-
-      queryClient.invalidateQueries({ queryKey: ['useGetProjetoById'], exact: false });
-
-      queryClient.invalidateQueries({ queryKey: ['useGetProjetosBySearch'], exact: false });
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['useGetProjetoItemById'], exact: false });
+      queryClient.invalidateQueries({ queryKey: ['useGetProjetoItensBySearch'], exact: false });
     },
   });
 }
