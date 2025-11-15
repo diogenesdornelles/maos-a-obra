ad1391b035fc6a39d35e69375862f442f309c443
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:17 -0300

    feat: add success handler to invalidate queries after updating endereco

--- web/src/hooks/queries/enderecos/useUpdateEndereco.ts
+++ web/src/hooks/queries/enderecos/useUpdateEndereco.ts
@@ -1,10 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 import { enderecosApi } from '@/api/enderecosApi';
 import { UpdateEndereco } from '@/types/enderecos/update';
+import { queryClient } from '@/services/queryClient';
 
 export function useUpdateEndereco() {
 	return useMutation({ 
 		mutationFn: ({ body, id }: { body: Partial<UpdateEndereco>; id: string }) => 
-			enderecosApi.updateEndereco(body, id) 
+			enderecosApi.updateEndereco(body, id),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetEnderecosBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetEnderecoById'], exact: false });
+		}
 	});
 }
