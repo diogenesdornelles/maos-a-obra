08b7009e5fcde6ce90118859d18bf174ca9cabdc
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-15 10:59:41 -0300

    refactor: update validation for CEP and make fields nullable in updateEnderecoSchema

--- web/src/features/(platform)/enderecos/alterar/validations/alterarEnderecoForm.ts
+++ web/src/features/(platform)/enderecos/alterar/validations/alterarEnderecoForm.ts
@@ -3,15 +3,22 @@ import { z } from 'zod';
 export const updateEnderecoSchema = z.object({
   bairroId: z.string().uuid('Id bairro não informado').optional(),
   logradouro: z.string().min(1, 'Logradouro é obrigatório').optional(),
-  numero: z.string().optional(),
-  complemento: z.string().optional(),
+  numero: z.string().optional().nullable(),
+  complemento: z.string().optional().nullable(),
   pais: z.string().optional(),
   cep: z
     .string()
     .optional()
-    .refine((val) => !val || val.length === 8, {
-      message: 'O CEP deve ter 8 caracteres se informado',
-    }),
+    .nullable()
+    .refine(
+      (val) => {
+        if (!val || val === '') return true; // Permite vazio ou null
+        return val.length === 8; // Se tiver valor, valida 8 dígitos
+      },
+      {
+        message: 'O CEP deve ter 8 caracteres',
+      }
+    ),
   status: z.boolean().optional(),
 });
 
