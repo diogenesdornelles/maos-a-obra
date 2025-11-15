609e32f1fde3c14701d4b6107c0f48912ca41145
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:10 -0300

    feat: add success handler to invalidate client queries after updating client

--- web/src/hooks/queries/clients/useUpdateCliente.ts
+++ web/src/hooks/queries/clients/useUpdateCliente.ts
@@ -2,10 +2,15 @@
 import { useMutation } from '@tanstack/react-query';
 import { clientesApi } from '@/api/clientesApi';
 import { UpdateCliente } from '@/types/clientes/update';
+import { queryClient } from '@/services/queryClient';
 
 export function useUpdateCliente() {
 	return useMutation({ 
 		mutationFn: ({ body, id }: { body: Partial<UpdateCliente>; id: string }) => 
-			clientesApi.updateCliente(body, id) 
+			clientesApi.updateCliente(body, id),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetClientesBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetClienteById'], exact: false });
+		}
 	});
 }
