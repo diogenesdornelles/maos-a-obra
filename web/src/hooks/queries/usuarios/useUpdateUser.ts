fa986a6e28005fcc34687a7476b88471f7821d08
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:39 -0300

    feat: add success handler to invalidate queries after updating user

--- web/src/hooks/queries/usuarios/useUpdateUser.ts
+++ web/src/hooks/queries/usuarios/useUpdateUser.ts
@@ -1,10 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import { usersApi } from '@/api/usuarioApi';
 import { UpdateUser } from '@/types/usuarios/update';
+import { queryClient } from '@/services/queryClient';
 
 export function useUpdateUser() {
     return useMutation({ 
         mutationFn: ({ body, id }: { body: Partial<UpdateUser>; id: string }) => 
-            usersApi.updateUser(body, id) 
+            usersApi.updateUser(body, id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['useGetMe'], exact: false });
+        }
     });
 }
