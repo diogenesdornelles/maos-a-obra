c9802652952823725682095b54e7bd6ef92b4707
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:06 -0300

    feat: implement mutation success handler to invalidate client queries after creation

--- web/src/hooks/queries/clients/usePostCreateCliente.ts
+++ web/src/hooks/queries/clients/usePostCreateCliente.ts
@@ -1,9 +1,14 @@
 import { clientesApi } from '@/api/clientesApi';
+import { queryClient } from '@/services/queryClient';
 import { CreateCliente } from '@/types/clientes/create';
 import { useMutation } from '@tanstack/react-query';
 
 export const usePostCreateCliente = () => {
   return useMutation({
     mutationFn: (clientData: CreateCliente) => clientesApi.createCliente(clientData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['useGetClientesBySearch'], exact: false });
+      queryClient.invalidateQueries({ queryKey: ['useGetClienteById'], exact: false });
+    }
   });
 };
