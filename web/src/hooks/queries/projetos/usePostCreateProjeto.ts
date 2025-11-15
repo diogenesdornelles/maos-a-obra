b74a376c8c198d454c78419a06a39ed00294af91
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:32 -0300

    feat: add success handler to invalidate queries after creating projeto item

--- web/src/hooks/queries/projetos/usePostCreateProjeto.ts
+++ web/src/hooks/queries/projetos/usePostCreateProjeto.ts
@@ -1,9 +1,14 @@
 import { projetosApi } from '@/api/projetosApi';
+import { queryClient } from '@/services/queryClient';
 import { CreateProjeto } from '@/types/projetos/create';
 import { useMutation } from '@tanstack/react-query';
 
 export const usePostCreateProjeto = () => {
   return useMutation({
     mutationFn: (projetoData: CreateProjeto) => projetosApi.createProjeto(projetoData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['useGetProjetosBySearch'], exact: false });
+      queryClient.invalidateQueries({ queryKey: ['useGetProjetoById'], exact: false });
+    }
   });
 };
