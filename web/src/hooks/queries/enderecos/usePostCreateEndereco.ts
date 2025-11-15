13e096ce9826dd9fe3064bd2a3bb47205c478acb
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:14 -0300

    feat: add success handler to invalidate queries after creating endereco

--- web/src/hooks/queries/enderecos/usePostCreateEndereco.ts
+++ web/src/hooks/queries/enderecos/usePostCreateEndereco.ts
@@ -1,9 +1,14 @@
 import { enderecosApi } from '@/api/enderecosApi';
+import { queryClient } from '@/services/queryClient';
 import { CreateEndereco } from '@/types/enderecos/create';
 import { useMutation } from '@tanstack/react-query';
 
 export const usePostCreateEndereco = () => {
   return useMutation({
     mutationFn: (body: CreateEndereco) => enderecosApi.createEndereco(body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['useGetEnderecosBySearch'], exact: false });
+      queryClient.invalidateQueries({ queryKey: ['useGetEnderecoById'], exact: false });
+    }
   });
 };
