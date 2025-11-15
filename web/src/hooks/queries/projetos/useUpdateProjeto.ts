6e168e8f9af390cc8fd33e948f96410384da0e47
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-11-14 21:35:35 -0300

    feat: add success handler to invalidate queries after updating projeto item

--- web/src/hooks/queries/projetos/useUpdateProjeto.ts
+++ web/src/hooks/queries/projetos/useUpdateProjeto.ts
@@ -1,10 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 import { projetosApi } from '@/api/projetosApi';
 import { UpdateProjeto } from '@/types/projetos/update';
+import { queryClient } from '@/services/queryClient';
 
 export function useUpdateProjeto() {
 	return useMutation({ 
 		mutationFn: ({ body, id }: { body: Partial<UpdateProjeto>; id: string }) => 
-			projetosApi.updateProjeto(body, id) 
+			projetosApi.updateProjeto(body, id),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['useGetProjetosBySearch'], exact: false });
+			queryClient.invalidateQueries({ queryKey: ['useGetProjetoById'], exact: false });
+		}
 	});
 }
